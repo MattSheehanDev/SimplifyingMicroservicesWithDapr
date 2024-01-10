c77856da19aba9d24405cb0b628393b7f0676b49
Matt Sheehan <matt-sheehan@outlook.com>
2024-01-10 09:17:50 -0500

    azurite

--- 04.bindings/azurite.ps1
+++ 04.bindings/azurite.ps1
@@ -1,1 +1,1 @@
-docker run -p 10000:10000 -p 10001:10001 -p 10002:10002 --name azurite -d -v ~/azurite:/data mcr.microsoft.com/azure-storage/azurite
+docker run -p 10000:10000 -p 10001:10001 -p 10002:10002 --name dapr_azurite -d -v ~/azurite:/data mcr.microsoft.com/azure-storage/azurite
